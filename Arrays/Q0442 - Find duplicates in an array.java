6db48fe62a3d64826bcc2e35bcb12e8a14064760
AJAYK1511 <mithraajay3@gmail.com>
2023-07-06 18:34:58 +0530

    Create Q0442 - Find duplicates in an array.java

---
+++ Arrays/Q0442 - Find duplicates in an array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> l=new LinkedList<Integer>();
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+            l.add(nums[i]);
+            while(nums[i]!=nums[i+1]){
+            i++;}
+            }
+        }
+        return l;
+    }
+}
