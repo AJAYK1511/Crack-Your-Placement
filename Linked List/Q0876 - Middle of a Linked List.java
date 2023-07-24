1dd49f0ce8d2960228852efa8b36a43c57786c36
AJAYK1511 <mithraajay3@gmail.com>
2023-07-24 18:49:18 +0530

    Create Q0876 - Middle of a Linked List.java

---
+++ Linked List/Q0876 - Middle of a Linked List.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+      int count = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            ++count;
+            temp = temp.next;
+        }
+        temp = head;
+        int middle = count / 2;
+        while (middle-- > 0) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
